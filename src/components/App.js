8187f3720f5506bbd3f121955f997ea2dcb80209
KaustubhD <kaustubhd287@gmail.com>
2018-10-27 21:56:34 +0530

    Login working

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navigation from './Navigation.js'
 import './App.css'
+
+import * as routes from '../constants/routes.js'
+import Login from './Login/login.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <Navigation />
+          <div>
+            <Navigation />
+            <hr />
+            <Route exact path={routes.LOG_IN} component={Login}/>
+          </div>
         </Router>
       </div>
     )
--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
-import './login.css'
+// import './login.css'
 
 class Login extends Component {
   render() {
     return (
-      <div className="login-container">
-        <form class="login-form">
-          <div class="email-container">
-            <input type="email" placeholder="Enter email"/>
-          </div>
-          <div class="password-container">
-            <input type="password" placeholder="Enter password"/>
-          </div>
-          <input type="submit" value="Log In" />
-          <a href="" class="forgot-but">Forgot Password</a>
-        </form>
+      <div>
+        <div className="login-container">
+          <form class="login-form">
+            <div class="email-container">
+              <input type="email" placeholder="Enter email"/>
+            </div>
+            <div class="password-container">
+              <input type="password" placeholder="Enter password"/>
+            </div>
+            <input type="submit" value="Log In" />
+            <a href="" class="forgot-but">Forgot Password</a>
+          </form>
+        </div>
+        <p>Dont have an account ? 
+          <a href="">Sign Up</a>
+        </p>
       </div>
-      <p>Don't have an account ? <a href="">Sign Up</a></p>
     )
   }
 }
