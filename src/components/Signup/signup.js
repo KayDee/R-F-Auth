f72c1b0f977cacb42db521d1fe3324123cebea16
KaustubhD <kaustubhd287@gmail.com>
2018-10-31 23:17:48 +0530

    Minor changes

--- src/components/Signup/signup.js
+++ src/components/Signup/signup.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import * as routes from '../constants/routes'
+
 // import './signup.css'
 
-class Signup extends Component {
-  render() {
-    return (
-      <div>
-        <div className="signup-container">
-          <h2>Sign up page</h2>
-          <form class="signup-form">
-            <div class="name-container">
-              <input type="text" placeholder="Enter name"/>
-            </div>
-            <div class="email-container">
-              <input type="email" placeholder="Enter email"/>
-            </div>
-            <div class="password-container">
-              <input type="password" placeholder="Enter password"/>
-            </div>
-            <input type="submit" value="Signup" />
-          </form>
-        </div>
-      </div>
+const INIT_STATE = {
+  username: '',
+  email: '',
+  passone: '',
+  passtwo: '',
+  error: null
+}
+
+const Signup = () => {
+  <div class="signup-container">
+    <h2>Sign up page</h2>
+    <SignupForm />
+  </div>
+}
+
+class SignupForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {...INIT_STATE}
+  }
+
+  onSubmit = (event) => {
+
+  }
+  render(){
+    return(
+      <form onSubmit={this.onSubmit}></form>
     )
   }
+  // render() {
+  //   return (
+  //     <div>
+  //       <div className="signup-container">
+  //         <h2>Sign up page</h2>
+  //         <form class="signup-form">
+  //           <div class="name-container">
+  //             <input type="text" placeholder="Enter name"/>
+  //           </div>
+  //           <div class="email-container">
+  //             <input type="email" placeholder="Enter email"/>
+  //           </div>
+  //           <div class="password-container">
+  //             <input type="password" placeholder="Enter password"/>
+  //           </div>
+  //           <input type="submit" value="Signup" />
+  //         </form>
+  //       </div>
+  //     </div>
+  //   )
+  // }
+}
+
+const SignupLink = () => {
+  <p>
+    Don\'t have an account ?{' '}
+    <Link to={routes.SIGN_UP}></Link>
+  </p>
 }
 
 export default Signup
+
+export{
+  SignupForm,
+  SignupLink
+}
